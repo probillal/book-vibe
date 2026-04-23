76dcda31cd7c14ea5276e2d9f078b62edc65b761
probillal <smbillalhossain273@gmail.com>
2026-04-23 20:53:36 +0600

    added navbar from daisyUi

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,21 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import Books from "../Pages/Books/Books";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <h2>Main</h2>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "books",
+        Component: Books,
+      },
+    ],
   },
 ]);
