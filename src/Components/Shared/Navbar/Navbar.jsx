f6c23d18467a5b476712af8ec4210a583a109172
probillal <smbillalhossain273@gmail.com>
2026-04-24 09:54:11 +0600

    margin fixed banner and navbar

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm container mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,17 +1,15 @@
 import bannerImage from "./../../assets/BannerImage.png";
 const Banner = () => {
   return (
-    <div className="container mx-auto my-8">
-      <div className="hero bg-base-200 min-h-[70vh] rounded-2xl ">
-        <div className="hero-content flex-col lg:flex-row-reverse w-full gap-32">
-          <img src={bannerImage} className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold">
-              Books to freshen up <br /> your bookshelf
-            </h1>
+    <div className="hero bg-base-200 min-h-[70vh] rounded-2xl container mx-auto my-8 ">
+      <div className="hero-content flex-col lg:flex-row-reverse w-full gap-32">
+        <img src={bannerImage} className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">
+            Books to freshen up <br /> your bookshelf
+          </h1>
 
-            <button className="btn btn-success mt-6">View The List</button>
-          </div>
+          <button className="btn btn-success mt-6">View The List</button>
         </div>
       </div>
     </div>
